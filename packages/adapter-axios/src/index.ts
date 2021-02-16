1e05f3083d342a28d2dab8ac1a1907bb5643fa2b
Dusan Kmet <morlokdk@gmail.com>
2021-02-16 18:26:49 +0100

    feat(adapter-axios): implement withOptions

--- packages/adapter-axios/src/index.ts
+++ packages/adapter-axios/src/index.ts
@@ -1,4 +1,4 @@
-import { AxiosError, AxiosInstance, AxiosResponse, Method } from 'axios'
+import { AxiosError, AxiosInstance, AxiosResponse, Method, AxiosRequestConfig } from 'axios'
 
 export interface AdapterResponse<T> extends AxiosResponse<T> {
 	status: number
@@ -16,17 +16,28 @@ export interface AdapterError<T> extends AxiosError<T> {
 export class AxiosAdapter {
 	constructor(
 		public axios: AxiosInstance,
+		public options: AxiosRequestConfig = {},
 	) {
 	}
 
-	async request<T = any>(method: string, path: string, body?: any, query?: any, options?: any): Promise<AdapterResponse<T>> {
+	withOptions(options: AxiosRequestConfig): AxiosAdapter {
+		return new AxiosAdapter(this.axios, this.createOptions(options))
+	}
+
+	createOptions(options: AxiosRequestConfig): AxiosRequestConfig {
+		return Object.assign({}, this.options, options, {
+			headers: Object.assign({}, this.options.headers || {}, options.headers || {}),
+		})
+	}
+
+	async request<T = any>(method: string, path: string, body?: any, query?: any, options?: AxiosRequestConfig): Promise<AdapterResponse<T>> {
 		try {
 			return await this.axios.request({
 				method: method as Method,
 				url: path,
 				data: body,
 				params: query,
-				...(options || {}),
+				...this.createOptions(options || {}),
 				validateStatus: status => status < 300,
 			})
 		} catch (err) {
