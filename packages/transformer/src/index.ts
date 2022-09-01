be4bc10e48426ea06e2c075800588ebb95c3586c
Dušan Kmeť <dusan@smartsupp.com>
2022-09-01 16:43:10 +0200

    chore: update dependencies

--- packages/transformer/src/index.ts
+++ packages/transformer/src/index.ts
@@ -7,3 +7,19 @@ export * from './transformer'
 export function transform(spec: OpenAPIV3.Document, options: TransformOptions = {}): CompileData.Data {
 	return new Transformer(spec).transform(options)
 }
+
+declare global {
+	namespace OpenAPIV3 {
+		interface SchemaObject {
+			'x-namespace': string
+			'x-generator-namespace': string
+			'x-generator-type': string
+		}
+		interface BaseSchemaObject {
+			'x-enum-names': string[]
+		}
+		interface OperationObject {
+			'x-generator-response-nullable': string
+		}
+	}
+}
